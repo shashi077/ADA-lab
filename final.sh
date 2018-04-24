3ad9e816dd733425dd89305231534dd382f683af
shashi077 <35594077+shashi077@users.noreply.github.com>
2018-04-24 10:45:14 +0530

    Create final.sh

---
+++ final.sh
@@ -0,0 +1,53 @@
+echo "eneter the no of pages "
+read n
+echo " enter the  page no "
+for ((i=1;i<=n;i++))
+do
+                read a[$i];
+        done
+        echo " enter the no frames "
+        read s
+        for ((i=0;i<s;i++))
+        do
+                (( frame[$i]= -1))
+                                j=0
+                                        echo " ref string \t page frames \n "
+                                done
+                                for ((i=1;i<=n;i++))
+                                do
+                                                echo " a[$i] "
+                                                        avail=0
+                                                done
+                                                for ((k=0;k<s;k++))
+                                                do
+                                                         if [[ frame[$k] -eq "${a[i]}" ]]
+                                                                  then
+                                                                           avail=1;
+                                                                            fi
+                                                                             if [ "$avail" -eq 0 ]
+
+   then
+
+            frame[j]=a[i];
+
+             ((j++))
+
+              let j=j%s
+
+               ((count++))
+
+                for ((k=0;k<s;k++))
+
+                         do
+                          echo " frame"                                                          
+
+                                   echo " page fault is "
+
+                                    echo " $count "
+
+
+
+                                      done
+
+                              fi
+
